aff37cd6a015d481f13670b00fa32e8d7a426fe9
zhumadil <zhumadil@MacBook-Air-zhumadil.local>
2023-01-17 17:51:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String [] args) {
+        String[] array = {"Nurila", "Samara", "Eliza"};
+
+
+        for (String a : array) {
+
+            System.out.println(a+" i love you ");
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
