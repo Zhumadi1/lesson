1efedbbada0203be2136e359e48d01ff3b75ad5f
Zhumadi1 <dzumaabdykadyrov@gmail.com>
2023-01-17 17:58:46 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String [] args) {
-        String[] array = {, "Samara", "Eliza"};
+        String[] array = { "Samara", "Eliza"};
 
 
         for (String a : array) {
